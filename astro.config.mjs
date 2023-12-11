949f830a55db84eeca43e11e7f1de7dd064e45fb
Nick-vr <nvanroyen@gmail.com>
2023-12-11 22:15:20 +0100

    updated astro etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
-
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify()
-});
+  integrations: [svelte()],
+})
